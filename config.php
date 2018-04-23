6f963bacbf403eef9f8dadb69059bf427d7f4e25
Ismail <ismail.elshaarawy@gmail.com>
2018-04-23 13:43:02 +0200

    modifying some files

--- config.php
+++ config.php
@@ -6,15 +6,8 @@ define('CMS_URI', 'http://localhost');
 // ID_KEY onesignal.com  my application
 /*
 */
-define('APPID', '334c02d1-c267-41e2-ab22-7ae2658ed06f');
-define('APKEY', 'MGI5ZmExOTYtYTI1NS00NmNlLTgwNzQtNWEyZmI2NjJhYmUw');
-
-
-/*   Ahmed Nagy's app        */
-/*
-define('APPID', '53419cbc-63b5-4c82-a4f9-aebf8b3930a6');
-define('APKEY', 'MzA5YmQxNWUtOWNlZS00MDk1LThkOGYtODk3ZDY2YjYwZWU5');
-*/
+define('APPID', '');
+define('APKEY', '');
 
 // Start massege
 define('ACTIONMESSAGE', 'We\'d like to show you notifications for the latest news.');
